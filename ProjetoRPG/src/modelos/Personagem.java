ae25973fd05f65b2b00b47a58d36e2d4e3e4480d
Z400\Jonas <jonasdhein@gmail.com>
2018-08-07 11:30:49 -0300

    Commit Inicial

---
+++ ProjetoRPG/src/modelos/Personagem.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author Jonas
+ */
+public class Personagem {
+    
+    private String classe;
+    private String nome;
+    private int vida;
+    private int ataque;
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    
+    
+    
+}
